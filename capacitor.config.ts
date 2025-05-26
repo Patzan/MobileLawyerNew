b8ed258369059c16bdcad58723afe6194f2a34b4
Daniel <danielaba@court.gov.il>
2025-05-26 17:12:39 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NGCS.MobileLawyerN.App',
+  webDir: 'www'
+};
+
+export default config;
